2e659e387c69efc9b69bbdb2074125a83730f6c7
MaazKhan-29 <maazk8448@gmail.com>
2026-04-30 02:02:41 +0530

    .

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkgs-3jXIGGKRiX-jbKc3HXdoLQVLTyAI",
-  authDomain: "mediai-4b446.firebaseapp.com",
-  projectId: "mediai-4b446",
-  storageBucket: "mediai-4b446.firebasestorage.app",
-  messagingSenderId: "457091631207",
-  appId: "1:457091631207:web:dd49e157c36730a20facc2",
-  measurementId: "G-GZJ8FEXQHE",
+  apiKey: "AIzaSyCEcBkZG_Vw2_Q33X1BmOtFKa4Xz2uNK_c",
+  authDomain: "mediai-6bd85.firebaseapp.com",
+  projectId: "mediai-6bd85",
+  storageBucket: "mediai-6bd85.firebasestorage.app",
+  messagingSenderId: "1391724626",
+  appId: "1:1391724626:web:28588548a4eaf7f9c5b78b"
 };
 
 const app = initializeApp(firebaseConfig);
